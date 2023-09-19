993bfa9a3b905e59e4e2b45511b2f4d8554355af
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2023-09-19 10:58:06 +0200

    add makeHeader component
    generates header on page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
+import "./styles/main.scss";
 
+const content = document.querySelector(".content");
+
+const makeHeader = () => {
+  let header = document.createElement("header");
+  let h1 = document.createElement("h1");
+  h1.textContent = "Sakura Sushi & Izakaya";
+
+  let nav = document.createElement("nav");
+  let ul = document.createElement("ul");
+  let listItems = ["Home", "Menu", "Contact"];
+  for (const item of listItems) {
+    let li = document.createElement("li");
+    li.textContent = item;
+    ul.append(li);
+  }
+  header.append(h1, ul);
+
+  return header;
+};
+
+content.append(makeHeader());
