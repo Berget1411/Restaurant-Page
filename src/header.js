ba5c9d8736dcfe2a4ba67c4653c51c2fa73acb08
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2023-09-19 19:55:06 +0200

    move makeHeader to new module

---
+++ src/header.js
@@ -0,0 +1,21 @@
+const makeHeader = () => {
+  let header = document.createElement("header");
+  let h1 = document.createElement("h1");
+  h1.textContent = "Sakura Sushi";
+
+  let nav = document.createElement("nav");
+  let ul = document.createElement("ul");
+  let listItems = ["Home", "Menu", "Contact"];
+  for (const item of listItems) {
+    let li = document.createElement("li");
+    li.textContent = item;
+
+    ul.append(li);
+  }
+
+  header.append(h1, ul);
+
+  return header;
+};
+
+export default makeHeader;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,6 @@
 import "./styles/main.scss";
+import makeHeader from "./header";
 
 const content = document.querySelector(".content");
-
-const makeHeader = () => {
-  let header = document.createElement("header");
-  let h1 = document.createElement("h1");
-  h1.textContent = "Sakura Sushi";
-
-  let nav = document.createElement("nav");
-  let ul = document.createElement("ul");
-  let listItems = ["Home", "Menu", "Contact"];
-  for (const item of listItems) {
-    let li = document.createElement("li");
-    li.textContent = item;
-    ul.append(li);
-  }
-  header.append(h1, ul);
-
-  return header;
-};
 
 content.append(makeHeader());
