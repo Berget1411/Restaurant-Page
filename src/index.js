b1895eb8241e298af8132ecc931ee1aa1d8bdcdd
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2023-09-20 13:25:36 +0200

    current select of nav gets an active-color class

--- src/index.js
+++ src/index.js
@@ -10,6 +10,26 @@ const home = makeHome();
 const menu = makeMenu();
 const contact = makeContact();
 
+const changeActiveNav = (id) => {
+  let homeNav = document.querySelector(`#nav1`);
+  let menuNav = document.querySelector(`#nav2`);
+  let contactNav = document.querySelector(`#nav3`);
+
+  if (id == "nav1") {
+    homeNav.classList.add("active-color");
+    menuNav.classList.remove("active-color");
+    contactNav.classList.remove("active-color");
+  } else if (id == "nav2") {
+    homeNav.classList.remove("active-color");
+    menuNav.classList.add("active-color");
+    contactNav.classList.remove("active-color");
+  } else {
+    homeNav.classList.remove("active-color");
+    menuNav.classList.remove("active-color");
+    contactNav.classList.add("active-color");
+  }
+};
+
 const makeHeader = () => {
   let header = document.createElement("header");
   let h1 = document.createElement("h1");
@@ -18,10 +38,11 @@ const makeHeader = () => {
   let nav = document.createElement("nav");
   let ul = document.createElement("ul");
   let listItems = ["Home", "Menu", "Contact"];
-  const counter = 0;
+  let counter = 1;
   for (const item of listItems) {
     let li = document.createElement("li");
     li.textContent = item;
+    li.setAttribute("id", `nav${counter}`);
     li.addEventListener("click", (e) => {
       if (e.target.textContent == "Home") {
         home.classList.remove("not-active");
@@ -36,9 +57,11 @@ const makeHeader = () => {
         menu.classList.add("not-active");
         contact.classList.remove("not-active");
       }
+      changeActiveNav(e.target.id);
     });
 
     ul.append(li);
+    counter++;
   }
 
   header.append(h1, ul);
