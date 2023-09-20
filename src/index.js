ab7ba690c9b6b680c4f29dcb895cd80374381ccd
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2023-09-20 08:31:06 +0200

    add makeHeader and makeFooter to index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,46 @@
 import "./styles/main.scss";
-import makeHeader from "./header";
 import makeHome from "./home";
-import makeFooter from "./footer";
+import icon from "./assets/github-mark.svg";
 
 const content = document.querySelector(".content");
+
+const makeHeader = () => {
+  let header = document.createElement("header");
+  let h1 = document.createElement("h1");
+  h1.textContent = "Sakura Sushi";
+
+  let nav = document.createElement("nav");
+  let ul = document.createElement("ul");
+  let listItems = ["Home", "Menu", "Contact"];
+  for (const item of listItems) {
+    let li = document.createElement("li");
+    li.textContent = item;
+
+    ul.append(li);
+  }
+
+  header.append(h1, ul);
+
+  return header;
+};
+
+const makeFooter = () => {
+  const footer = document.createElement("footer");
+  const text = document.createElement("p");
+  text.textContent = "Copyright © 2023 Berget";
+  const link = document.createElement("a");
+  link.href = "https://github.com/Berget1411";
+  const img = document.createElement("img");
+  img.src = icon;
+
+  link.append(img);
+
+  footer.append(text, link);
+
+  return footer;
+};
+
+export default makeFooter;
 
 const container = document.createElement("section");
 container.classList.add("container");
